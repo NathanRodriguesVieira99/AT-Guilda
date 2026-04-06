2a8180773e421216d47d9b9be0578feaa84bf313
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-06 19:05:52 -0300

    fix: adiciona annotation para o ID

--- src/main/java/com/edu/infnet/tp1/domain/models/painelTaticoMissao/PainelTaticoMissao.java
+++ src/main/java/com/edu/infnet/tp1/domain/models/painelTaticoMissao/PainelTaticoMissao.java
@@ -6,6 +6,8 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -19,6 +21,7 @@ public class PainelTaticoMissao implements Serializable {
 
   @Id
   @Column(name = "missao_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long missaoId;
 
   @Column(name = "titulo")
