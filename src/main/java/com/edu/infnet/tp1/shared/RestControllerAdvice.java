0ecaa48f639aa6c14f9d6f58a320402d125e40b1
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-15 13:18:34 -0300

    refactor(exceptions): remove exceptions repetitivas de erros nas classes para usar as exceptions genéricas

--- src/main/java/com/edu/infnet/tp1/shared/RestControllerAdvice.java
+++ src/main/java/com/edu/infnet/tp1/shared/RestControllerAdvice.java
@@ -7,47 +7,31 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.edu.infnet.tp1.shared.errors.ErrorMessage;
-import com.edu.infnet.tp1.shared.exceptions.AventureiroInvalidParamsException;
-import com.edu.infnet.tp1.shared.exceptions.AventureiroNotFoundException;
-import com.edu.infnet.tp1.shared.exceptions.CompanheiroInvalidParamsException;
-import com.edu.infnet.tp1.shared.exceptions.InvalidQueryParamException;
-import com.edu.infnet.tp1.shared.exceptions.MissaoNotFoundException;
+import com.edu.infnet.tp1.shared.errors.exceptions.InvalidParamsException;
+import com.edu.infnet.tp1.shared.errors.exceptions.InvalidQueryParamsException;
+import com.edu.infnet.tp1.shared.errors.exceptions.ResourceNotFoundException;
 
 // @ControllerAdvice -> Serve para padronizar as Exceptions para evitar TryCatch  nos controllers
 // Se não houver exceptions customizadas no service, o controller vai lancar a padrão do Java
 @ControllerAdvice
 public class RestControllerAdvice extends ResponseEntityExceptionHandler {
-  @ExceptionHandler(AventureiroInvalidParamsException.class)
-  private ResponseEntity<ErrorMessage> aventureiroInvalidParamsHandler(AventureiroInvalidParamsException exception) {
-    ErrorMessage errorMessage = new ErrorMessage(HttpStatus.UNAUTHORIZED,
-        "O Aventureiro foi criado com parametros inválidos.");
-    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorMessage);
+  @ExceptionHandler(InvalidParamsException.class)
+  private ResponseEntity<ErrorMessage> aventureiroInvalidParamsHandler(InvalidParamsException exception) {
+    ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST,
+        "Parametros inválidos!");
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
   }
 
-  @ExceptionHandler(AventureiroNotFoundException.class)
-  private ResponseEntity<ErrorMessage> aventureiroNotFoundExceptionHandler(AventureiroNotFoundException exception) {
-    ErrorMessage errorMessage = new ErrorMessage(HttpStatus.NOT_FOUND, "O aventureiro não foi encontrado.");
+  @ExceptionHandler(ResourceNotFoundException.class)
+  private ResponseEntity<ErrorMessage> ResourceNotFoundExceptionHandler(ResourceNotFoundException exception) {
+    ErrorMessage errorMessage = new ErrorMessage(HttpStatus.NOT_FOUND, "O recurso não foi encontrado!");
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
   }
 
-  @ExceptionHandler(InvalidQueryParamException.class)
-  private ResponseEntity<ErrorMessage> invalidPageNumberException(InvalidQueryParamException exception) {
-    ErrorMessage errorMessage = new ErrorMessage(HttpStatus.UNAUTHORIZED, "Query Params inválidos!");
-
-    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorMessage);
+  @ExceptionHandler(InvalidQueryParamsException.class)
+  private ResponseEntity<ErrorMessage> invalidQueryParamsException(InvalidQueryParamsException exception) {
+    ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST, "Query Params inválidos!");
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
   }
 
-  @ExceptionHandler(CompanheiroInvalidParamsException.class)
-  private ResponseEntity<ErrorMessage> invalidCompanheiroParamsException(CompanheiroInvalidParamsException exception) {
-    ErrorMessage errorMessage = new ErrorMessage(HttpStatus.UNAUTHORIZED,
-        "O Companheiro foi criado com parametros inválidos.");
-
-    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorMessage);
-  }
-
-  @ExceptionHandler(MissaoNotFoundException.class)
-  private ResponseEntity<ErrorMessage> missaoNotFoundException(MissaoNotFoundException exception) {
-    ErrorMessage errorMessage = new ErrorMessage(HttpStatus.NOT_FOUND, "A missão não foi encontrada.");
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
-  }
 }
